e13d1836378bb6d9b835e785892dc3ad5439ab28
SaiDurga-28 <147792374+SaiDurga-28@users.noreply.github.com>
2025-11-04 20:11:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else Statement/the-else-statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // code here
+    int n;
+    scanf("%d",&n);
+    if(n>100)
+    {
+        printf("Big");
+    }
+    else
+    {
+        printf("Number");
+    }
+
+    return 0;
+}
