a570741b75c95dfc7413435adea7ff9a825c7d65
SaiDurga-28 <147792374+SaiDurga-28@users.noreply.github.com>
2025-11-05 18:53:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // code here
+    int n;
+    scanf("%d",&n);
+    if(n>100)
+    {
+        printf("Big \n");
+    }
+    printf("Number");
+
+    return 0;
+}
