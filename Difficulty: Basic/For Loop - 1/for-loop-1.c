c75d970cf322e601e4792d2a0b9e67496d313455
SaiDurga-28 <147792374+SaiDurga-28@users.noreply.github.com>
2025-11-06 23:53:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    // use for loop to print the
+    // multiplication table of n
+    // code here
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d ",n*i);
+    }
+
+    return 0;
+}
